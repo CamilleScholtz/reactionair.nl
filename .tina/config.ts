a55d61e2a299db80363ebe3e6d350cd9496a9052
Camille Scholtz <admin@camille.sh>
2022-12-19 23:31:10 +0100

    Tina?

---
+++ .tina/config.ts
@@ -0,0 +1,44 @@
+import { defineConfig } from "tinacms";
+
+// Your hosting provider likely exposes this as an environment variable
+const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "main";
+
+export default defineConfig({
+  branch,
+  clientId: "575f9e63-32ef-43ec-854b-8b83e1d6369c",
+  token: "1363ce5db3a28b35ac51098d2d6048ac818a2d5a",
+  build: {
+    outputFolder: "admin",
+    publicFolder: "static",
+  },
+  media: {
+    tina: {
+      mediaRoot: "uploads",
+      publicFolder: "static",
+    },
+  },
+  schema: {
+    collections: [
+      {
+        name: "artikel",
+        label: "Artikelen",
+        path: "content/artikelen",
+        fields: [
+          {
+            type: "string",
+            name: "title",
+            label: "Title",
+            isTitle: true,
+            required: true,
+          },
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body",
+            isBody: true,
+          },
+        ],
+      },
+    ],
+  },
+});
