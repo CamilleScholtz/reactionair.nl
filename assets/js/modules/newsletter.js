f2e4f89f917212f8e591d9595af64ee4c3bf7324
Camille Scholtz <admin@camille.sh>
2023-07-03 23:24:42 +0200

    Allow multiple newsletter containers

--- assets/js/modules/newsletter.js
+++ assets/js/modules/newsletter.js
@@ -1,29 +1,31 @@
 export const newsletter = (main) => {
-	const newsletter = main.querySelector("#newsletter-container");
-	if (!newsletter) {
+	const newsletters = main.querySelectorAll(".newsletter-container");
+	if (!newsletters) {
 		return;
 	}
 
-	newsletter.querySelector("form").addEventListener("submit", (ev) => {
-		ev.preventDefault();
+	newsletters.forEach((newsletter) => {
+		newsletter.querySelector("form").addEventListener("submit", (ev) => {
+			ev.preventDefault();
 
-		const data = new FormData(ev.target);
+			const data = new FormData(ev.target);
 
-		fetch(ev.target.action, {
-			method: "POST",
-			headers: {
-				"Content-Type": "application/json"
-			},
-			body: data
+			fetch(ev.target.action, {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json"
+				},
+				body: data
+			});
+
+			ev.target.reset();
+
+			const envelope           = newsletter.querySelector(".cover");
+			envelope.src             = envelope.dataset.send;
+			envelope.style.transform = "translateY(-10px) scale(0.9)";
+
+			const text     = newsletter.querySelector(".text p");
+			text.innerHTML = "Wij sturen u een e-mail ter verificatie, zie uw inbox.";
 		});
-
-		ev.target.reset();
-
-		const envelope           = newsletter.querySelector(".cover");
-		envelope.src             = envelope.dataset.send;
-		envelope.style.transform = "translateY(-10px) scale(0.9)";
-
-		const text     = newsletter.querySelector(".text p");
-		text.innerHTML = "Wij sturen u een e-mail ter verificatie, zie uw inbox.";
 	});
 }
