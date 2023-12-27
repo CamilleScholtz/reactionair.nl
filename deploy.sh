ed0717819f164c3d70499fa0beda045272a1f19f
Camille Scholtz <admin@camille.sh>
2023-12-27 15:44:02 +0100

    Update deploy script

--- deploy.sh
+++ deploy.sh
@@ -5,5 +5,8 @@ if [ "$1" = "--pull" ]; then
 fi
 
 hugo -s "/usr/local/www/reactionair.nl" --cleanDestinationDir -d "/usr/local/www/reactionair.nl/build"
-rm -r "/usr/local/www/reactionair.nl/public"
+
+if [ -d "/usr/local/www/reactionair.nl/public" ]; then
+	rm -rf "/usr/local/www/reactionair.nl/public"
+fi
 mv "/usr/local/www/reactionair.nl/build" "/usr/local/www/reactionair.nl/public"
